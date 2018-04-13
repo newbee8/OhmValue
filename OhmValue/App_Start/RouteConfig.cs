9022b6100a9ebb315c3ae9e720d057abf6893883
newbee8 <36909026+newbee8@users.noreply.github.com>
2018-04-12 22:42:25 -0500

    Route Correction

--- OhmValue/App_Start/RouteConfig.cs
+++ OhmValue/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace OhmValue
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "OhmValue", action = "OhmValueCalculator", id = UrlParameter.Optional }
             );
         }
     }
